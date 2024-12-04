e289a364219af5b6f1060cbeba429cab2343ef18
Haoyi Shi(Mac) <shi00317@umn.edu>
2024-12-03 22:46:35 -0600

    the prompt for diffusion model will be fine-tuned by GPT

--- diffusion/img2img/img2img_web.py
+++ diffusion/img2img/img2img_web.py
@@ -3,6 +3,10 @@ import requests
 import torch
 from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 import argparse
+import base64
+from openai import OpenAI
+
+client = OpenAI()
 torch.manual_seed(1234)
 model_id = "timbrooks/instruct-pix2pix"
 pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id,cache_dir="./model", torch_dtype=torch.float32, safety_checker=None)
@@ -16,11 +20,44 @@ def input_preprocessing(path):
     image = image.convert("RGB")
     image = image.resize((512, 512))
     return image
-
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')  
 def main(img_path,prompt,steps,guidance):
     image = input_preprocessing(img_path)
+    base64_image = encode_image(img_path)
 
-    images = pipe(prompt, image=image, num_inference_steps=steps,guidance_scale=guidance, image_guidance_scale=1.1).images
+    prompt=[
+        {"role": "system", "content": '''You are an stable-diffusion prompt summarizer. Your job is to create a well estiblished prompt based on the user input key words and input image. Important is the new prompt need to be limited to 77 tokens!}
+            '''}
+    ]
+    prompt.append(
+        {
+            "role": "user",
+            "content": [
+            {
+                "type": "text",
+                "text": f"{prompt}",
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                "url":  f"data:image/jpeg;base64,{base64_image}"
+                },
+            },
+            ],
+        }
+        )
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=prompt,
+    )
+
+    # Get the response content
+    new_prompt = response.choices[0].message.content
+    print(new_prompt)
+    images = pipe(new_prompt, image=image, num_inference_steps=steps,guidance_scale=guidance, image_guidance_scale=1.1).images
     images[0].save(img_path[:-4]+"_new.png")
 
 if __name__=="__main__":
